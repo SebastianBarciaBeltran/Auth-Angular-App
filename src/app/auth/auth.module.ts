8295305233e33042943580bff50a9b3ade3834f6
SebastianBarciaBeltran <sebastianbarciabeltran@gmail.com>
2023-01-20 18:43:54 +0100

    created the componets in module of auth

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './views/login/login.component';
+import { RegisterComponent } from './views/register/register.component';
+import { MainComponent } from './views/main/main.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    RegisterComponent,
+    MainComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
