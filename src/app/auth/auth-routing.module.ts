223141452b6e1e951ec1160fb26f22ac2cadf684
SebastianBarciaBeltran <sebastianbarciabeltran@gmail.com>
2023-01-20 18:53:28 +0100

    implemented the lazyload in module auth

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './views/main/main.component';
+import { LoginComponent } from './views/login/login.component';
+import { RegisterComponent } from './views/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+        {
+          path: 'login', component: LoginComponent
+        },
+        {
+          path: 'register', component: RegisterComponent
+        },
+        {
+          path: '**',
+          redirectTo: 'login'
+        }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
